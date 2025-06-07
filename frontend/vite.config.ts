3ae111b260650998733726426d1c7881c7f18cc6
Yatin <yatinsharma811@gmail.com>
2025-06-08 03:44:22 +0530

    From https to http connection between frontend & backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:7032",
+        target: "http://localhost:5196",
         changeOrigin: true,
         secure: false,
       },
